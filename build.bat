2ee8c80a810447fded26e73d1629feae2e7e9142
Qriist <qriist@gmail.com>
2024-12-08 19:08:44 -0500

    updated .gitignore

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-msbuild sqlite3mc_vc17.sln /t:Clean /p:Configuration=Release /p:Platform=Win64 /p:DefineConstants="SQLITE3MC_USE_MINIZ=1;SQLITE_ENABLE_COMPRESS=1;SQLITE_ENABLE_SQLAR=1;SQLITE_ENABLE_ZIPFILE=1"
-msbuild sqlite3mc_vc17.sln /p:Configuration=Release /p:Platform=Win64 /p:DefineConstants="SQLITE3MC_USE_MINIZ=1;SQLITE_ENABLE_COMPRESS=1;SQLITE_ENABLE_SQLAR=1;SQLITE_ENABLE_ZIPFILE=1"
+msbuild sqlite3mc_vc17.sln /t:Clean /p:Configuration=Release /p:Platform=Win64 /p:DefineConstants="SQLITE3MC_USE_MINIZ=1;SQLITE_ENABLE_COMPRESS=1;SQLITE_ENABLE_SQLAR=1;SQLITE_ENABLE_ZIPFILE=1" /verbosity:diagnostic /fileLogger
+msbuild sqlite3mc_vc17.sln /p:Configuration=Release /p:Platform=Win64 /p:DefineConstants="SQLITE3MC_USE_MINIZ=1;SQLITE_ENABLE_COMPRESS=1;SQLITE_ENABLE_SQLAR=1;SQLITE_ENABLE_ZIPFILE=1" /verbosity:diagnostic /fileLogger
